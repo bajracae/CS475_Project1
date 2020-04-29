e49a883854692619695b20663c52a9ada25d2abc
Aeijan Bajracharya <bajracae@flip3.engr.oregonstate.edu>
2020-04-28 22:33:23 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CS475_Project1
+How to run the program:
+Enter "bash makefile"
+
+The program will automatically run with different number of threads and trials.
--- project1.cpp
+++ project1.cpp
@@ -8,12 +8,12 @@
 
 // setting the number of threads:
 #ifndef NUMT	
-#define NUMT	8
+#define NUMT	1
 #endif
 
 // setting the number of trials in the monte carlo simulation:
 #ifndef NUMTRIALS
-#define NUMTRIALS	500000
+#define NUMTRIALS	1
 #endif
 
 // how many tries to discover the maximum performance:
